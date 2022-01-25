114b4558865b1a6c7d9a136924bdfad2240573da
PriyankaPerumal1 <90901203+PriyankaPerumal1@users.noreply.github.com>
2022-01-25 21:12:31 +0530

    Create stub_alerter.py

---
+++ stub_alerter.py
@@ -0,0 +1,9 @@
+
+def network_alert_stub(celcius):
+    print(f'ALERT: Temperature is {celcius} celcius')
+    # Return 200 for ok
+    # Return 500 for not-ok
+    if(celcius < float(100)):
+      return 200
+    else
+      return 500
